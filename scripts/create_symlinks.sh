b8a532161e61aaee1387d4fd8026b5f7602ed54c
Erik Fasterius <erik.fasterius@outlook.com>
2019-08-26 13:18:59 +0200

    Add script to create all relevant symlinks

---
+++ scripts/create_symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script that creates symbolic links for all relevant files in the repository
+ln -s ~/.bash/profiles/bashrc ~/.bashrc
+ln -s ~/.bash/profiles/bash_profile ~/.bash_profile
+ln -s ~/.bash/git/gitconfig ~/.gitconfig
