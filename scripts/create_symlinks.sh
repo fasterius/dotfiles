2bc5109e66cdd5641e1a6d751d056d6e5bd730e9
Erik Fasterius <erik.fasterius@outlook.com>
2020-03-02 10:39:27 +0100

    Add Conda config file to repository and symlink

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Script that creates symbolic links for all relevant files in the repository
+
+# Bash
 ln -s ~/.bash/profiles/bashrc ~/.bashrc
 ln -s ~/.bash/profiles/bash_profile ~/.bash_profile
+
+# Conda
+ln -s ~/.bash/profiles/condarc ~/.condarc
+
+# Git
 ln -s ~/.bash/git/gitconfig ~/.gitconfig
 ln -s ~/.bash/git/gitignore_global ~/.gitignore_global
