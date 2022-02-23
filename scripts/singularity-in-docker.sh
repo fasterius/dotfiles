2c6fa51a0976bb1db35766be962e932c98108df0
Erik Fasterius <erik.fasterius@outlook.com>
2022-02-23 10:27:54 +0100

    Add script for building Singularity images
    Add script for building Singularity images from local Docker images
    using the `kaczmarj/singularity-in-docker` repository.

---
+++ scripts/singularity-in-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script that runs the "Singularity in Docker" to build Singularity images from
+# local Docker images (https://github.com/kaczmarj/singularity-in-docker). Input
+# is a local Docker image as an argument to this script, which outputs a
+# Singularity image file with the same name, plus the `.sif` extension.
+
+# Check number of input arguments
+if [ "$#" -ne 1 ]; then
+    echo "Must provide exactly one input Docker image"
+    exit 1
+fi
+
+# Get input Docker image
+DOCKER_IMAGE=$1
+
+# Convert to Singularity image
+docker run \
+    --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):/work \
+    kaczmarj/singularity:3.8.0 \
+    build ${DOCKER_IMAGE}.sif docker-daemon://${DOCKER_IMAGE}:latest
