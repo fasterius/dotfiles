2ce3526ab863c09cab5462eff812a6d26ff5a66d
Erik Fasterius <erik.fasterius@outlook.com>
2023-01-22 11:15:53 +0100

    Also ensure treesitter for bash is installed

--- nvim/init.lua
+++ nvim/init.lua
@@ -523,7 +523,7 @@ require('nvim-tmux-navigation').setup {
 require('nvim-treesitter.configs').setup {
 
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'lua', 'python', 'r', 'markdown', 'help', 'vim' },
+    ensure_installed = { 'lua', 'python', 'r', 'bash', 'markdown', 'help', 'vim' },
 
     -- General settings
     highlight = { enable = true },
