3f8fe4e159366c984dc4bd980cf19a9ea323aca5
Erik Fasterius <erik.fasterius@outlook.com>
2023-02-03 11:44:37 +0100

    Add telescope highlight searcher

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,20 +2,19 @@
 return {
     'nvim-telescope/telescope.nvim',
     branch       = '0.1.x',
-    lazy = true,
-    module = true,
     keys = {
-        { '<leader>fg' },
-        { '<leader>ff' },
-        { '<leader>fb' },
-        { '<leader>sh' },
-        { '<leader>sd' },
-        { '<leader>/'  },
-        { '<leader>sg' },
-        { '<leader>sw' },
-        { 'gr'         },
-        { '<leader>ds' },
-        { '<leader>ws' }
+        '<leader>ff',
+        '<leader>fg',
+        '<leader>fb',
+        '<leader>sg',
+        '<leader>sw',
+        '<leader>sd',
+        '<leader>sh',
+        '<leader>si',
+        '<leader>/' ,
+        '<leader>ds',
+        '<leader>ws',
+        'gr'
     },
     dependencies = {
         'nvim-lua/plenary.nvim',
@@ -43,8 +42,6 @@ return {
             }
         }
 
-        local telescope = require('telescope.builtin')
-
         -- Change colour of search results
         -- TODO: don't hard-code
         vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = '#268bd2' })
@@ -53,25 +50,31 @@ return {
         pcall(require('telescope').load_extension, 'fzf')
 
         -- Functions for keymaps
+        local builtin = require('telescope.builtin')
         local function _live_grep()
-            telescope.live_grep { cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] }
+            builtin.live_grep { cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] }
         end
         local function _grep_string()
-            telescope.grep_string { cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] }
+            builtin.grep_string { cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] }
         end
 
-        -- Keymaps
-        vim.keymap.set('n', '<leader>fg', telescope.git_files,                     { desc = '[F]ind files inside [G]it repository'            })
-        vim.keymap.set('n', '<leader>ff', telescope.find_files,                    { desc = '[F]ind [F]iles in the current working directory' })
-        vim.keymap.set('n', '<leader>fb', telescope.buffers,                       { desc = '[F]ind existing [B]uffers'                       })
-        vim.keymap.set('n', '<leader>sh', telescope.help_tags,                     { desc = '[S]earch [H]elp'                                 })
-        vim.keymap.set('n', '<leader>sd', telescope.diagnostics,                   { desc = '[S]earch [D]iagnostics'                          })
-        vim.keymap.set('n', '<leader>/',  telescope.current_buffer_fuzzy_find,     { desc = '[/] Fuzzily search in current buffer]'           })
-        vim.keymap.set('n', '<leader>sg', _live_grep,                              { desc = "[S]earch inside [G]it repository "               })
-        vim.keymap.set('n', '<leader>sw', _grep_string,                            { desc = '[S]earch current [W]ord inside Git repository'   })
-        vim.keymap.set('n', 'gr',         telescope.lsp_references,                { desc = '[G]oto [R]eferences'                             })
-        vim.keymap.set('n', '<leader>ds', telescope.lsp_document_symbols,          { desc = '[D]ocument [S]ymbols'                            })
-        vim.keymap.set('n', '<leader>ws', telescope.lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols'                           })
+        -- Finding files
+        vim.keymap.set('n', '<leader>fg', builtin.git_files,                     { desc = '[F]ind files inside [G]it repository'            })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files,                    { desc = '[F]ind [F]iles in the current working directory' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers,                       { desc = '[F]ind existing [B]uffers'                       })
+
+        -- Searching
+        vim.keymap.set('n', '<leader>sg', _live_grep,                            { desc = "[S]earch inside [G]it repository "             })
+        vim.keymap.set('n', '<leader>sw', _grep_string,                          { desc = '[S]earch current [W]ord inside Git repository' })
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags,                     { desc = '[S]earch [H]elp'                               })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics,                   { desc = '[S]earch [D]iagnostics'                        })
+        vim.keymap.set('n', '<leader>si', builtin.highlights,                    { desc = '[S]earch h[I]ghlights'                         })
+        vim.keymap.set('n', '<leader>/',  builtin.current_buffer_fuzzy_find,     { desc = '[/] Fuzzily search in current buffer]'         })
+
+        -- LSP
+        vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols,          { desc = '[D]ocument [S]ymbols'  })
+        vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
+        vim.keymap.set('n', 'gr',         builtin.lsp_references,                { desc = '[G]oto [R]eferences'   })
 
     end
 }
