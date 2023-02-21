69ca45993ca71bd1a17e542f8bf9f8ae707e2b6d
Erik Fasterius <erik.fasterius@outlook.com>
2023-02-21 12:44:20 +0100

    Use `lua_ls` instead of `sumneko_lua` (deprecated)

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -66,7 +66,7 @@ return {
             on_attach = on_attach,
             capabilities = capabilities,
         }
-        lspconfig.sumneko_lua.setup {
+        lspconfig.lua_ls.setup {
             on_attach = on_attach,
             capabilities = capabilities,
             settings = {
