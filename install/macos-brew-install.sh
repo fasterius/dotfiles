eafb54387b9093e63815acdd364e4231e8bf63e1
Erik Fasterius <erik.fasterius@outlook.com>
2023-03-10 11:00:46 +0100

    Add script to install Homebrew + formulae on MacOS

---
+++ install/macos-brew-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Homebrew non-interactively
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install formulae
+brew install \
+    bash \
+    bat \
+    black \
+    coreutils \
+    gcc \
+    git \
+    graphviz \
+    imagemagick \
+    neovim \
+    openjdk@17 \
+    pandoc \
+    readline \
+    ripgrep \
+    tmux \
+    tree-sitter \
+    vim \
+    wget
+
+# Install casks
+brew install --cask \
+    font-meslo-lg-nerd-font
