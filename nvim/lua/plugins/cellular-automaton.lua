895c4273f94464261de7b2c31add4e76dabdc10f
Erik Fasterius <erik.fasterius@outlook.com>
2023-04-27 15:15:51 +0200

    Remove `cellular-automaton` Neovim plugin

--- nvim/lua/plugins/cellular-automaton.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'Eandrju/cellular-automaton.nvim',
-    keys = {
-        { '<leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>' },
-        { '<leader>gol', '<cmd>CellularAutomaton game_of_life<CR>' }
-    }
-}
