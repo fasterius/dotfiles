6dd20b460139cbfa04ad0e7232ba0a76f29739ac
Erik Fasterius <erik.fasterius@outlook.com>
2024-03-15 15:29:51 +0100

    Add keymap for sourcing current file

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -38,3 +38,6 @@ vim.keymap.set('n', '[d',        vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d',        vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Neovim development
+vim.keymap.set('n', '<localleader>x', ':source %<CR>')
