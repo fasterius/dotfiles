d82d6d2230950b394d356fd74a8f6056ce6377aa
Erik Fasterius <erik.fasterius@outlook.com>
2024-03-15 15:29:20 +0100

    Add `helpful.vim` plugin

---
+++ nvim/lua/plugins/helpful.lua
@@ -0,0 +1,6 @@
+-- Finds the (Neo)Vim version a command was introduced in, useful for finding
+-- the required (Neo)Vim version for writing plugins
+return {
+    'tweekmonster/helpful.vim',
+    cmd = 'HelpfulVersion',
+}
