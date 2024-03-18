8e88b64c32a793caf56e4d23b2c1ab508210afc2
Erik Fasterius <erik.fasterius@outlook.com>
2024-03-18 14:38:55 +0100

    Add auto-formatting with `conform.nvim`

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+-- Formatting
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				markdown = { "prettier" },
+				python = { "ruff" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
