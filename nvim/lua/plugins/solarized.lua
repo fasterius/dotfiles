f4c17edb431676b1acd4e358240cb7bdce077a21
Erik Fasterius <erik.fasterius@outlook.com>
2025-01-30 14:56:27 +0100

    Fix markdown bold highlight

--- nvim/lua/plugins/solarized.lua
+++ nvim/lua/plugins/solarized.lua
@@ -20,6 +20,7 @@ return {
         vim.api.nvim_set_hl(0, "@label", { fg = "#657B83", bold = true })
         vim.api.nvim_set_hl(0, "@variable.parameter.r", { fg = "#657B83" })
         vim.api.nvim_set_hl(0, "@markup.italic", { fg = "#657B83", italic = true })
+        vim.api.nvim_set_hl(0, "@markup.strong", { fg = "#657B83", bold = true })
     end,
 }
 
