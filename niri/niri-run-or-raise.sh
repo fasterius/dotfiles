d02680bdacaf0a02b0e02300699e3948e3827850
Erik Fasterius <erik.fasterius@outlook.com>
2025-08-17 12:02:58 +0200

    Add cycling to `niri-run-or-raise.sh` script

--- niri/niri-run-or-raise.sh
+++ niri/niri-run-or-raise.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
+# A script to focus, cycle or launch an application in Niri.
 #
-# A script to intelligently focus or launch an application in niri.
-# Designed to be used with keybindings.
+# - If the app is not running, it launches a new app instance.
+# - If the app is running but not focused, it focuses the app window.
+# - If the app is running, is currently focused AND has multiple instances,
+#   cycle through the different instances.
 #
-# - If the app is not running, it launches a new instance.
-# - If the app is running but not focused, it focuses the most recently used window.
-# - If 'allow_multi' is 'yes' AND the app is already focused, it launches a new instance.
+# Define key bindings in your Niri config that execute this script, for example:
+#   Super+F { spawn "~/.config/niri/niri-run-or-raise.sh" "firefox"; }
 #
 # Dependencies:
+# - bash: The script uses Bash arrays.
 # - niri: The Wayland compositor this script is designed for.
-# - jq: A command-line JSON processor.
+# - jq:   A command-line JSON processor.
 
 # Arguments
-APP_CLASS="$1"     # Application's app_id (e.g. firefox)
-APP_CMD="$2"       # Command to run the application (e.g. firefox; optional)
+APP_CLASS="$1" # Application's app_id (e.g. firefox)
+APP_CMD="$2"   # Command to run the application (e.g. firefox; optional)
 
-# Check if required arguments are provided
+# Check if the required arguments are provided, exit otherwise
 if [ -z "$APP_CLASS" ] ; then
     notify-send -t 5000 "Usage: niri-run-or-raise.sh <APP_CLASS> <APP_CMD>"
     exit 1
 fi
 
-# Get info about the focused window and existing windows
+# Get the ID of the currently focused window
 FOCUSED_ID=$(niri msg -j focused-window | jq -r '.id')
 
-# Find the ID of the last window matching the app_class.
-# This is assumed to be the most recently used one.
-ALL_MATCHING_IDS=$(\
+# Find windows matching the app class and read them into an array
+readarray -t MATCHING_IDS < <(
     niri msg -j windows \
     | jq -r --arg app_class "$APP_CLASS" \
     '
@@ -36,36 +38,33 @@ ALL_MATCHING_IDS=$(\
         | .id
     '
 )
-MATCHING_ID=$( \
-    printf '%s\n' "$ALL_MATCHING_IDS" \
-    | head -n 1
-)
 
-# # Get first and last matching ID
-# NEWEST_MATCH=$( \
-#     printf '%s\n' "$ALL_MATCHING_IDS" \
-#     | sort -r \
-#     | head -n 1
-# )
-# OLDEST_MATCH=$( \
-#     printf '%s\n' "$ALL_MATCHING_IDS" \
-#     | sort -r \
-#     | tail -n 1
-# )
-
-# Run or raise, as applicable
-if [ -z "$ALL_MATCHING_IDS" ]; then
-
-    # Set app command to app class if not specified
+# Launch the app and exit the script if the number of matching windows is zero
+if [ ${#MATCHING_IDS[@]} -eq 0 ]; then
+    # Use the app class as the command if no app command is supplied
     if [ -z "$APP_CMD" ]; then
         APP_CMD="$APP_CLASS"
     fi
-    bash -c "$APP_CMD" &
-
-# elif [ "$FOCUSED_ID" = "$APP_CLASS" ]; then
-    # $APP_CMD &
-
-else
-    # Focus app with matching ID
-    niri msg action focus-window --id "$MATCHING_ID"
+    "$APP_CMD" &
+    exit 0
 fi
+
+# Find the array index of the currently focused window
+CURRENT_INDEX=-1
+for INDEX in "${!MATCHING_IDS[@]}"; do
+    if [ "${MATCHING_IDS[$INDEX]}" = "$FOCUSED_ID" ]; then
+        CURRENT_INDEX=$INDEX
+        break
+    fi
+done
+
+# Cycle to the next matching array index if the currently focused ID was found
+# in the array, otherwise set the target index to zero
+if [ $CURRENT_INDEX -ge 0 ]; then
+    TARGET_INDEX=$(( (CURRENT_INDEX + 1) % ${#MATCHING_IDS[@]} ))
+else
+    TARGET_INDEX=0
+fi
+
+# Switch focus to the target window stored in the array
+niri msg action focus-window --id "${MATCHING_IDS[$TARGET_INDEX]}"
