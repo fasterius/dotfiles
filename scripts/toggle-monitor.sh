90c415cc62fb215370db103a747c0ab8ef2566a0
Erik Fasterius <erik.fasterius@outlook.com>
2025-09-05 20:10:02 +0200

    Add script to toggle second monitor on/off

---
+++ scripts/toggle-monitor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script that toggles a monitor on/off
+
+# Monitor name
+MONITOR="DP-3"
+
+# Check if monitor is on/off and toggle
+if wlr-randr | grep -A6 "$MONITOR" | grep "Enabled" | grep -q "yes"; then
+    wlr-randr --output "$MONITOR" --off
+else
+    wlr-randr --output "$MONITOR" --on --preferred
+fi
