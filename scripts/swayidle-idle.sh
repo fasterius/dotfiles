514ba1e6afb0cc22b495b8a6b8ae266b66ec84a6
Erik Fasterius <erik.fasterius@outlook.com>
2025-09-10 12:01:32 +0200

    Increase lock/suspend wait time

--- scripts/swayidle-idle.sh
+++ scripts/swayidle-idle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 swayidle -w \
-	timeout 300 '~/.dotfiles/scripts/swaylock-lock.sh' \
-    timeout 600 'systemctl suspend' \
+	timeout 600 '~/.dotfiles/scripts/swaylock-lock.sh' \
+    timeout 900 'systemctl suspend' \
 	before-sleep '~/.dotfiles/scripts/swaylock-lock.sh'
