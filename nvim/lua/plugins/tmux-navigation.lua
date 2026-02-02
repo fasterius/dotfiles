42dbb68392f5d423d0e882a71a3161c6981805fd
Erik Fasterius <erik.fasterius@outlook.com>
2026-02-02 12:39:19 +0100

    Use `vim-tmux-navigator` and disable nvim wrapping

--- nvim/lua/plugins/tmux-navigation.lua
+++ nvim/lua/plugins/tmux-navigation.lua
@@ -1,18 +1,17 @@
 -- Movement between Neovim and Tmux
 return {
-    "alexghergh/nvim-tmux-navigation",
+    "christoomey/vim-tmux-navigator",
+    keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
     config = function()
-        require("nvim-tmux-navigation").setup({
+        -- Disable movement when zoomed in to a pane
+        vim.cmd([[ let g:tmux_navigator_disable_when_zoomed = 1 ]])
 
-            keybindings = {
-                left = "<C-h>",
-                down = "<C-j>",
-                up = "<C-k>",
-                right = "<C-l>",
-            },
-
-            -- Disable movement when zoomed in to a pane
-            disable_when_zoomed = true,
-        })
+        -- Disable edge wrapping
+        vim.cmd([[ let  g:tmux_navigator_no_wrap = 1 ]])
     end,
 }
