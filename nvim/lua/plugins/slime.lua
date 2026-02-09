0e416d2f452af3023de28282635c0b815ab0b4cd
Erik Fasterius <erik.fasterius@outlook.com>
2026-02-09 08:40:36 +0100

    Send newlines with cells/selections with vim-slime

--- nvim/lua/plugins/slime.lua
+++ nvim/lua/plugins/slime.lua
@@ -30,13 +30,18 @@ return {
         -- details at https://cirw.in/blog/bracketed-paste
         vim.g.slime_bracketed_paste = 1
 
-        -- General mappings
-        vim.keymap.set("n", "<localleader>s", "<plug>SlimeMotionSend")
+        -- General mappings (simple)
         vim.keymap.set("n", "<localleader>l", "<plug>SlimeLineSend")
-        vim.keymap.set("x", "<localleader>v", "<plug>SlimeRegionSend")
-        vim.keymap.set("n", "<localleader>c", "<plug>SlimeSendCell")
+        vim.keymap.set("n", "<localleader>s", "<plug>SlimeMotionSend")
         vim.keymap.set("n", "<localleader>C", ':SlimeSend0 "\\x03"<CR>')
         vim.keymap.set("n", "<localleader>q", ':SlimeSend0 "q"<CR>')
         vim.keymap.set("n", "<localleader>r", ':SlimeSend0 "\\n\\n"<CR>')
+
+        -- General mappings (also send two newlines to properly finish Python
+        -- code; not needed for other languages, but doesn't do anything
+        -- either). The `SlimeMotionSend` command cannot be sent like this
+        -- because it interferes with the operator-pending mode.
+        vim.keymap.set("n", "<localleader>c", '<Plug>SlimeSendCell<Cmd>SlimeSend0 "\\n\\n"<CR>')
+        vim.keymap.set("x", "<localleader>v", '<plug>SlimeRegionSend<Cmd>SlimeSend0 "\\n\\n"<CR>')
     end,
 }
