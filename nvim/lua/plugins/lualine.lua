94d9f5a69e707da0cbc979997fdf15e6ba6599df
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-14 17:10:11 +0200

    Use `mainly.nvim` + `simple-zoom.nvim` for Lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 -- Fancier statusline
 return {
     "nvim-lualine/lualine.nvim",
+    dependencies = "fasterius/mainly.nvim",
     config = function()
         local colours
         local theme
@@ -46,42 +47,18 @@ return {
             }
         end
 
-        -- Function to get the current filename: if the current filename equals
-        -- `main.nf` also include the parent directory, otherwise just return
-        -- the filename itself (useful for working with nf-core).
-        -- The filename modifiers used are:
-        --     :t   - "Tail", just the filename
-        --     :p   - "Path", the full file path
-        --     :h   - "Head", the everything except the filename
-        --     :h:t - A combination that gets only the parent directory
-        local function get_filename()
-            local filename = vim.fn.expand("%:t")
-            if filename == "main.nf" then
-                local filepath = vim.fn.expand("%:p")
-                local parent_dir = vim.fn.fnamemodify(filepath, ":h:t")
-                return parent_dir .. "/" .. filename
-            elseif filename == "main.nf.test" then
-                local filepath = vim.fn.expand("%:p")
-                local grandparent_dir = vim.fn.fnamemodify(filepath, ":h:h:t")
-                local parent_dir = vim.fn.fnamemodify(filepath, ":h:t")
-                return grandparent_dir .. "/" .. parent_dir .. "/" .. filename
-            else
-                return filename
-            end
-        end
-
         -- Functions for getting filename and colours when zoomed in using the
-        -- `simple-zoom.nvim` plugin
+        -- `simple-zoom.nvim` and `mainly.nvim` plugins
+        local mainly_filename = require("mainly").filename
         local function get_filename_is_zoomed_in()
-            local filename = get_filename()
             if vim.t["simple-zoom"] == nil then
-                return filename
-            elseif vim.t["simple-zoom"] == "zoom" then
-                return filename .. " 󰍉"
+                return mainly_filename()
+            elseif type(vim.t["simple-zoom"]) == "table" then
+                return mainly_filename() .. " 󰍉"
             end
         end
         local function get_colour_zoomed_in()
-            if vim.t["simple-zoom"] == "zoom" then
+            if type(vim.t["simple-zoom"]) == "table" then
                 return { bg = colours.zoomed_bg, fg = colours.zoomed_fg }
             else
                 return { bg = colours.unzoomed_bg }
@@ -102,7 +79,6 @@ return {
                 lualine_c = {
                     {
                         get_filename_is_zoomed_in,
-                        file_status = false,
                         color = get_colour_zoomed_in,
                     },
                     { "diff" },
@@ -117,7 +93,7 @@ return {
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = { { get_filename, file_status = false } },
+                lualine_c = { mainly_filename },
                 lualine_x = { "filetype" },
                 lualine_y = {},
                 lualine_z = {},
