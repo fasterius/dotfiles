fdadc6b5e55d1d07bcd807e7095ad874b0bf06d6
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-22 16:44:54 +0200

    Remove unused Neovim plugins

--- nvim/plugin/helpful.lua
+++
@@ -1,6 +0,0 @@
--- Finds the (Neo)Vim version a command was introduced in, useful for finding
--- the required (Neo)Vim version for writing plugins
--- No lazy loading; used to be lazily loaded on commands
-vim.pack.add({
-    "https://github.com/tweekmonster/helpful.vim",
-})
