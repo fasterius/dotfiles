266a5ea57b0aacf3809a428fb516c7274afa9573
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-23 10:56:54 +0200

    Remove shadowed `Ctrl-[hjkl]` keymaps
    Remove the shadowed `Ctrl-[hjkl]` keymaps from `keymaps.lua`, as they
    are superseded by the `tmux-navigation` plugin.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -30,12 +30,6 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Movement in splits
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
