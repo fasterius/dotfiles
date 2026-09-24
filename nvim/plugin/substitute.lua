f22f37567755f573ab8c70680e966ec623277ea4
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-24 17:37:13 +0200

    Remove `{ noremap = true}` in keymaps (is default)

--- nvim/plugin/substitute.lua
+++ nvim/plugin/substitute.lua
@@ -5,27 +5,12 @@ vim.pack.add({
 require("substitute").setup()
 
 -- Substitute
-vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
-vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
-vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
-vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+vim.keymap.set("n", "s", require("substitute").operator)
+vim.keymap.set("x", "s", require("substitute").visual)
+vim.keymap.set("n", "ss", require("substitute").line)
+vim.keymap.set("n", "S", require("substitute").eol)
 
 -- Substitute over range
-vim.keymap.set(
-    "n",
-    "<leader>s",
-    require("substitute.range").operator,
-    { noremap = true }
-)
-vim.keymap.set(
-    "x",
-    "<leader>s",
-    require("substitute.range").visual,
-    { noremap = true }
-)
-vim.keymap.set(
-    "n",
-    "<leader>ss",
-    require("substitute.range").word,
-    { noremap = true }
-)
+vim.keymap.set("n", "<leader>s", require("substitute.range").operator)
+vim.keymap.set("x", "<leader>s", require("substitute.range").visual)
+vim.keymap.set("n", "<leader>ss", require("substitute.range").word)
