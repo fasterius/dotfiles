8f23e31dbf9c5a3ebcc1d3a1448db24edcf9d606
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-24 17:40:55 +0200

    Use default config for `surround.nvim`

--- nvim/plugin/surround.lua
+++ nvim/plugin/surround.lua
@@ -1,18 +1,8 @@
 -- Surround with brackets, parentheses, quotes, etc. with Treesitter-support
--- No lazy loading; used to lazily loaded on commands
 -- Defer loading until startup is finished
 vim.schedule(function()
     vim.pack.add({
         "https://github.com/kylechui/nvim-surround",
     })
-    require("nvim-surround").setup({
-        aliases = {
-            -- Aliases to conform to how `targets.vim` work
-            ["b"] = { ")", "]", "}" },
-            ["q"] = { '"', "'", "`" },
-        },
-
-        -- Do not highlight selection
-        highlight = { false },
-    })
+    require("nvim-surround").setup()
 end)
